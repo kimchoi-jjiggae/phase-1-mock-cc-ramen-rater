8e127d38f42052d19f9f7dd33c3efe29354109d1
Michelle <choi.michelle.12@gmail.com>
2023-02-08 10:53:18 -0500

    completed core deliverables and post

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
 // write your code here
+let ramenMenu = document.getElementById("ramen-menu")
+let ramenDetail = document.getElementById("ramen-detail")
+let form = document.getElementById("new-ramen")
+
+document.addEventListener("DOMContentLoaded", e =>
+{
+        fetch("http://localhost:3000/ramens")
+            .then(res=> res.json())
+            .then(data => {
+                data.forEach(ramen =>{
+                    renderRamen(ramen)
+                    featureRamen(ramen)
+            })
+                featureRamen(data[0])
+            })
+})
+
+function renderRamen(ramen){
+    let ramenItem = document.createElement('img')
+    ramenItem.src = ramen.image
+    ramenMenu.append(ramenItem)
+    ramenItem.addEventListener("click", e=> featureRamen(ramen))
+
+}
+
+function featureRamen(ramen){
+    ramenDetail.getElementsByClassName("detail-image")[0].src = ramen.image
+    ramenDetail.getElementsByClassName("name")[0].innerText = ramen.name
+    ramenDetail.getElementsByClassName("restaurant")[0].innerText = ramen.restaurant
+    document.getElementById('rating-display').innerText = ramen.rating
+    document.getElementById('comment-display').innerText = ramen.comment
+}
+
+form.addEventListener("submit", e=> {
+    e.preventDefault()
+    object = {}
+    object['name'] = document.getElementById('new-name').value
+    object['restaurant'] = document.getElementById('new-restaurant').value
+    object['image'] = document.getElementById('new-image').value
+    object['rating'] = document.getElementById('new-rating').value
+    object['comment'] = document.getElementById('new-comment').value
+    console.log(object)
+
+    fetch('http://localhost:3000/ramens', {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(object)
+    })
+      .then(res=> res.json())
+      .then(data => renderRamen(data))
+})
